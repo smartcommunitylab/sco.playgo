fbe47080ab7cf7913e4464845c4c9f408a5ddabe
chin8 <matteo.chini@fbk.eu>
2019-10-16 17:13:07 +0200

    aiming prod

--- play&go-mobile/www/js/controllers/HomeCtrl.js
+++ play&go-mobile/www/js/controllers/HomeCtrl.js
@@ -549,7 +549,7 @@ angular.module('viaggia.controllers.home', [])
     $rootScope.currentUser = null;
     $scope.noStatus = false;
     $rootScope.profileImg = null;
-    $scope.tmpUrl = 'https://dev.smartcommunitylab.it/playandgo/gamificationweb/player/avatar/' + Config.getAppGameId() + '/'
+    $scope.tmpUrl = 'https://tn.smartcommunitylab.it/playandgo/gamificationweb/player/avatar/' + Config.getAppGameId() + '/'
     $scope.getImage = function () {
       if ($scope.status)
         profileService.getProfileImage($scope.status.playerData.playerId).then(function (image) {
