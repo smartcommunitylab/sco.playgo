bcabe467a5363ab99c9acf6f242cb06119c77462
kazhamiakin <kazhamiakin@gmail.com>
2019-10-17 14:02:33 +0200

    Fix for canInvite

--- playandgo/src/main/java/eu/trentorise/smartcampus/mobility/gamificationweb/StatusUtils.java
+++ playandgo/src/main/java/eu/trentorise/smartcampus/mobility/gamificationweb/StatusUtils.java
@@ -109,7 +109,7 @@ public class StatusUtils {
 		ps.setPointConcept(points);		
 		
 		Calendar c = Calendar.getInstance();
-		Calendar from = Calendar.getInstance(); from.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY); from.set(Calendar.HOUR_OF_DAY, 0); from.set(Calendar.MINUTE, 0); from.set(Calendar.SECOND, 0);
+		Calendar from = Calendar.getInstance(); from.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY); from.set(Calendar.HOUR_OF_DAY, 12); from.set(Calendar.MINUTE, 0); from.set(Calendar.SECOND, 0);
 		Calendar to = Calendar.getInstance(); to.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY); to.set(Calendar.HOUR_OF_DAY, 12); to.set(Calendar.MINUTE, 0); to.set(Calendar.SECOND, 0);
 		ps.setCanInvite(c.before(to) && c.after(from));
 		
