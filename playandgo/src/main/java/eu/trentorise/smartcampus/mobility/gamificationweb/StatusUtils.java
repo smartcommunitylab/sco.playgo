17e332b16fb3b48f184e7cac064d63d6f6176503
kazhamiakin <kazhamiakin@gmail.com>
2020-06-16 23:12:02 +0200

    enabled challenges

--- playandgo/src/main/java/eu/trentorise/smartcampus/mobility/gamificationweb/StatusUtils.java
+++ playandgo/src/main/java/eu/trentorise/smartcampus/mobility/gamificationweb/StatusUtils.java
@@ -1,9 +1,7 @@
 package eu.trentorise.smartcampus.mobility.gamificationweb;
 
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -110,12 +108,10 @@ public class StatusUtils {
 		points.removeIf(x -> !PC_GREEN_LEAVES.equals(x.getName()) || !PC_WEEKLY.equals(x.getPeriodType()));
 		ps.setPointConcept(points);		
 		
-		// challenges temporary disabled
-		ps.setCanInvite(false);
-//		Calendar c = Calendar.getInstance();
-//		Calendar from = Calendar.getInstance(); from.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY); from.set(Calendar.HOUR_OF_DAY, 12); from.set(Calendar.MINUTE, 0); from.set(Calendar.SECOND, 0);
-//		Calendar to = Calendar.getInstance(); to.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY); to.set(Calendar.HOUR_OF_DAY, 12); to.set(Calendar.MINUTE, 0); to.set(Calendar.SECOND, 0);
-//		ps.setCanInvite(c.before(to) && c.after(from));
+		Calendar c = Calendar.getInstance();
+		Calendar from = Calendar.getInstance(); from.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY); from.set(Calendar.HOUR_OF_DAY, 12); from.set(Calendar.MINUTE, 0); from.set(Calendar.SECOND, 0);
+		Calendar to = Calendar.getInstance(); to.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY); to.set(Calendar.HOUR_OF_DAY, 12); to.set(Calendar.MINUTE, 0); to.set(Calendar.SECOND, 0);
+		ps.setCanInvite(c.before(to) && c.after(from));
 		
 		return ps;
 	}
