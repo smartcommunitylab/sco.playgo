bb828190ddd3f38175b657d8e3de0e4bb8d2b3d1
kazhamiakin <kazhamiakin@gmail.com>
2020-10-20 10:03:05 +0200

    temp fix

--- playandgo/src/main/java/eu/trentorise/smartcampus/mobility/geolocation/model/TTDescriptor.java
+++ playandgo/src/main/java/eu/trentorise/smartcampus/mobility/geolocation/model/TTDescriptor.java
@@ -436,7 +436,10 @@ public class TTDescriptor {
 					.filter(a -> a.shape_pt_lat != null && a.shape_pt_lon != null)
 					.map(a -> new Geolocation(a.shape_pt_lat, a.shape_pt_lon, null))
 					.collect(Collectors.toList());
-			shape = TrackValidator.fillTrace(shape, 100.0 / 1000 / 2 / Math.sqrt(2));
+			
+			if (shape.size() > 0) {
+				shape = TrackValidator.fillTrace(shape, 100.0 / 1000 / 2 / Math.sqrt(2));
+			} 
 			shapeMap.put(id, shape);
 			
 		}
